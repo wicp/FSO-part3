789249692f71e6b7f05887d64f02f324e8f0b9a9
wicp <noneof@yourbusiness.com>
2021-04-02 16:53:31 +0100

    completed 3.3

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const app = express()
 
-const notes = require('./db.json')
+const phonebook = require('./db.json')
 
 app.get('/api/persons', (request,response) => {
-    response.json(notes)
+    response.json(phonebook)
+})
+
+app.get('/api/persons/:id', (request,response) => {
+    console.log(`person ${JSON.stringify(request.params)}`)
+    const id = Number(request.params.id)
+    const person = phonebook.persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request,response) => {
     response.send(`
-        <p>Phonebook has info for ${notes.persons.length} people</p>
+        <p>Phonebook has info for ${phonebook.persons.length} people</p>
         <p>${new Date()}</p>
     `)
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
