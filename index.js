ece14c806d7d866fc72a85f4636d4f597e491239
wicp <noneof@yourbusiness.com>
2021-04-02 16:30:50 +0100

    completed 3.2

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ app.get('/api/persons', (request,response) => {
     response.json(notes)
 })
 
+app.get('/info', (request,response) => {
+    response.send(`
+        <p>Phonebook has info for ${notes.persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
