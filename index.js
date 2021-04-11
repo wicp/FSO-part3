dcadbd7d1890f19d590793681b356a0ceeb04318
wicp <noneof@yourbusiness.com>
2021-04-11 13:41:43 +0100

    completed 3.15

--- index.js
+++ index.js
@@ -51,10 +51,14 @@ app.post("/api/persons", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  phonebook.persons = phonebook.persons.filter((person) => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 app.get("/info", (request, response) => {
